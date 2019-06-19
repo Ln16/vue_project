fd033e7483b55129107ed015d6725820f1b656fe
ln16 <572881252@qq.com>
2019-06-19 21:25:44 +0800

    完成搜索页面

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,6 @@
 import ajax from './ajax'
 const base = ''
+const apiBase = '/api'
 export const reqHomeData = () => ajax(base + '/mock/home')
+export const reqSearchInit = () => ajax(apiBase +'/xhr/search/init.json')
+export const reqMatching = (keywordPrefix) => ajax(apiBase + 'http://m.you.163.com/xhr/search/searchAutoComplete.json', {keywordPrefix})
--- src/pages/Search/Search.vue
+++ src/pages/Search/Search.vue
@@ -2,17 +2,63 @@
   <div class="container">
     <div class="search">
       <!-- 需要icon -->
-      <input type="text" placeholder="动态的数据">
-      <span class="goBack" @click="$router.back()">取消</span>
+      <div class="searchHeader">
+        <i class="iconfont icon-iconfontzhizuobiaozhun22"></i>
+        <input type="text" :placeholder="placeholder" v-model="searchText">
+        <span class="goBack" @click="$router.back()">取消</span>
+      </div>
+      <ul class="searchList">
+        <li class="searchItem" v-for="(item, index) in searchArr" :key="index">
+          {{item}}
+        </li>
+      </ul>
     </div>
-    <div class="associate">
-
+    <div class="associate" v-show="!searchText">
+      <p class="hot">热门搜索</p>
+      <ul class="hotList">
+        <li class="hotItem" v-for="(item, index) in hotKeywordVOList" :key="index">
+          {{item.keyword}}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import {reqSearchInit,reqMatching} from '../../api'
   export default {
+    data() {
+      return {
+        searchText:'',
+        placeholder:'',
+        hotKeywordVOList:[],
+        searchArr:[]
+      }
+    },
+    async mounted(){
+      const result = await reqSearchInit()
+      if(result.code==='200'){
+        this.placeholder = result.data.defaultKeyword.keyword
+        this.hotKeywordVOList = result.data.hotKeywordVOList
+      }
+      this.economy = false
+    },
+    watch: {
+      async searchText(){
+        //函数节流，减少请求次数
+        if (this.economy) {
+          return
+        }
+        this.economy = true
+        window.setTimeout(()=>{
+          this.economy = false
+        },300)
+        const result = await reqMatching(this.searchText)
+        this.searchArr = result.data
+        console.log(result);
+        
+      }
+    },
   }
 </script>
 
@@ -24,29 +70,67 @@
   .search
     width 100%
     height 88px
-    padding 0 30px
     box-sizing border-box
     line-height 88px
     background-color #fff
-    input 
-      background-color #F4F4F4
-      width 564px
-      height 56px
-      padding 0 20px
-      margin-right 25px
-      border none
-      border-radius 5px
-    .goBack 
-      display inline-block
-      width 56px
-      height 41px
-      text-align center
-      line-height 41px
-      font-size 28px
+    position relative
+    .searchHeader
+      padding 0 30px
+      i 
+        position absolute
+        top 0
+        left 50px
+        font-size 34px
+      input 
+        background-color #F4F4F4
+        width 514px
+        height 56px
+        padding 0 20px 0 70px
+        margin-right 25px
+        border none
+        border-radius 5px
+        &::-webkit-input-placeholder
+          font-size 26px
+          color #999
+      .goBack 
+        display inline-block
+        width 56px
+        height 41px
+        text-align center
+        line-height 41px
+        font-size 28px
+    .searchList
+      padding 0 40px
+      background-color #fff
+      .searchItem
+        height 100px
+        color #333
+        font-size 28px
+        line-height 100px
+        border-bottom 2px solid #F4F4F4
+        
   .associate
     width 100%
     height 404px
     padding 0 30px 30px 
     box-sizing border-box
     background-color #fff
+    .hot
+      width 100%
+      height 90px
+      font-size 28px
+      color #999
+      line-height 90px
+    .hotList
+      .hotItem
+        line-height 47px
+        padding 0 15px
+        margin 0 32px 32px 0
+        border 1.1px solid #cccccc
+        color #333
+        float left
+        border-radius 8px
+        &:first-child 
+          border 1.1px solid #b4282d
+          color #b4282d
 </style>
